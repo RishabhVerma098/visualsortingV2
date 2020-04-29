73724127ae922a0d11a15f66b2c5f89f229ecda1
rishabh verma <vermarishabh0987@gmail.com>
2020-04-30 01:44:32 +0530

    slider done

--- src/components/workingBox/TopControls/componentsResuable/slider/slider.js
+++ src/components/workingBox/TopControls/componentsResuable/slider/slider.js
@@ -1,9 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./slider.scss";
+//TODO:UNINSTALL REACT-SLIDER
 const CustomSlider = () => {
+  const [width, setWidth] = useState(40);
+
+  const changeWidth = (add) => {
+    if (add) {
+      if (width < 100) {
+        setWidth((prevWidth) => prevWidth + 5);
+      }
+    } else {
+      if (width > 5) {
+        setWidth((prevWidth) => prevWidth - 5);
+      }
+    }
+  };
+
   return (
     <div className="slider">
-      <p>slider</p>
+      <div className="plus" onClick={() => changeWidth(true)}>
+        <img src={require("../../../../../assets/plus.png")}></img>
+      </div>
+      <div className="range">
+        <div className="bar" style={{ width: `${width}%` }}></div>
+        <p>{width / 5}</p>
+      </div>
+      <div className="minus" onClick={() => changeWidth(false)}>
+        <img src={require("../../../../../assets/minus.png")}></img>
+      </div>
     </div>
   );
 };
