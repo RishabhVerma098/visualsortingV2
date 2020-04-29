ca63c4e2b2d005d1d8337ceddc02aba7472aa2b3
rishabh verma <vermarishabh0987@gmail.com>
2020-04-30 02:21:17 +0530

    bottom controls halway done

--- src/components/workingBox/bottomControls/bottomControls.js
+++ src/components/workingBox/bottomControls/bottomControls.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./bottomControls.scss";
 const BottomControls = () => {
-  return <div className="bottomControls"></div>;
+  return (
+    <div className="bottomControls">
+      <div className="controls">
+        <div className="back">
+          <img src={require("../../../assets/fast.png")} alt="back"></img>
+        </div>
+
+        <div className="pause">
+          <p>||</p>
+        </div>
+        <div className="fast">
+          <img src={require("../../../assets/fast.png")} alt="fast"></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BottomControls;
