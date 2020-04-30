0337facae8d4bd1f260d92c14c55c6b06e726b7c
rishabh verma <vermarishabh0987@gmail.com>
2020-04-30 16:41:52 +0530

    gradients bars decided and added in globals

--- src/components/workingBox/sortingBars/sortingBars.js
+++ src/components/workingBox/sortingBars/sortingBars.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./sortingBars.scss";
 const SortingBox = () => {
-  return <div className="sortingBox"></div>;
+  return (
+    <div className="sortingBox">
+      <div className="bar">
+        <div className="hollow">
+          <div className="fill">
+            <p>45</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SortingBox;
