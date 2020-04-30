bdbe777e4b03ce4cdef5c703eb205fcb1e62d026
rishabh verma <vermarishabh0987@gmail.com>
2020-04-30 22:58:33 +0530

    sorting done

--- src/components/workingBox/sortingBars/sortingBars.js
+++ src/components/workingBox/sortingBars/sortingBars.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./sortingBars.scss";
 import { random } from "lodash";
 import uuid from "react-uuid";
+import { motion } from "framer-motion";
 const SortingBox = () => {
-  const numberOfBars = 10;
+  const numberOfBars = 5;
   const [bars, setBars] = useState([]);
 
   useEffect(() => {
@@ -21,28 +22,126 @@ const SortingBox = () => {
     setBars([...initailBars]);
   }, []);
 
-  console.log(bars);
+  const spring = {
+    type: "spring",
+    damping: 20,
+    stiffness: 300,
+  };
+
+  const timeout = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  let bubbleSortOptimised = async () => {
+    let newBarList = bars;
+    let len = bars.length;
+    let swapped;
+    do {
+      swapped = false;
+      for (let i = 0; i < len - 1; i++) {
+        if (newBarList[i].height > newBarList[i + 1].height) {
+          newBarList[i].color.background =
+            "linear-gradient(to bottom, #36d1dc, #5b86e5)";
+          newBarList[i + 1].color.background =
+            "linear-gradient(to bottom, #36d1dc, #5b86e5)";
+          setBars([...newBarList]);
+          await timeout(500);
+          let tmp = newBarList[i];
+          newBarList[i] = newBarList[i + 1];
+          newBarList[i + 1] = tmp;
+          swapped = true;
+          newBarList[i].color.background =
+            "linear-gradient(to top, #1d976c, #93f9b9)";
+          newBarList[i + 1].color.background =
+            "linear-gradient(to top, #1d976c, #93f9b9)";
+          setBars([...newBarList]);
+          await timeout(350);
+          newBarList[i].color.background =
+            "linear-gradient(to bottom, #fdc830, #f37335)";
+          newBarList[i + 1].color.background =
+            "linear-gradient(to bottom, #fdc830, #f37335)";
+          setBars([...newBarList]);
+          await timeout(500);
+        }
+      }
+    } while (swapped);
+    setBars([...newBarList]);
+  };
   return (
-    <div className="sortingBox">
-      {bars.map((bar) => {
-        return (
-          <div className="bar" style={{ height: `${bar.height}%` }}>
-            <div className="hollow">
-              <div
-                className="fill"
-                style={{
-                  background: `${bar.color.background}`,
-                  boxShadow: `${bar.color.boxShadow}`,
-                }}
-              >
-                <p>{bar.height}</p>
+    <div className="sortingBox" onClick={bubbleSortOptimised}>
+      <ul className="hey">
+        {bars.map((bar) => {
+          return (
+            <motion.li
+              key={bar.id}
+              className="bar"
+              style={{ height: `${bar.height}%` }}
+              layoutTransition={spring}
+            >
+              <div className="hollow">
+                <div
+                  className="fill"
+                  style={{
+                    background: `${bar.color.background}`,
+                    boxShadow: `${bar.color.boxShadow}`,
+                  }}
+                >
+                  <p>{bar.height}</p>
+                </div>
               </div>
-            </div>
-          </div>
-        );
-      })}
+            </motion.li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
 export default SortingBox;
+
+// const bubbleSort = async () => {
+//   let newBarList = bars;
+//   let len = bars.length;
+//   let flag = true;
+//   for (let i = 0; i < len; i++) {
+//     for (let j = 0; j < len - 1; j++) {
+//       if (newBarList[j].height > newBarList[j + 1].height) {
+//         newBarList[j].color.background =
+//           "linear-gradient(to bottom, #36d1dc, #5b86e5)";
+//         newBarList[j + 1].color.background =
+//           "linear-gradient(to bottom, #36d1dc, #5b86e5)";
+//         setBars([...newBarList]);
+//         await timeout(500);
+//         let tmp = newBarList[j];
+//         newBarList[j] = newBarList[j + 1];
+//         newBarList[j + 1] = tmp;
+//         newBarList[j].color.background =
+//           "linear-gradient(to top, #1d976c, #93f9b9)";
+//         newBarList[j + 1].color.background =
+//           "linear-gradient(to top, #1d976c, #93f9b9)";
+//         setBars([...newBarList]);
+//         await timeout(350);
+//         newBarList[j].color.background =
+//           "linear-gradient(to bottom, #fdc830, #f37335)";
+//         newBarList[j + 1].color.background =
+//           "linear-gradient(to bottom, #fdc830, #f37335)";
+//         setBars([...newBarList]);
+//         await timeout(500);
+//         flag = false;
+//       }
+//       if (flag) {
+//         newBarList[j].color.background = "black";
+//         newBarList[j + 1].color.background = "black";
+//         setBars([...newBarList]);
+//         await timeout(350);
+//         newBarList[j].color.background =
+//           "linear-gradient(to bottom, #fdc830, #f37335)";
+//         newBarList[j + 1].color.background =
+//           "linear-gradient(to bottom, #fdc830, #f37335)";
+//         setBars([...newBarList]);
+//         await timeout(500);
+//       }
+//       flag = true;
+//     }
+//   }
+// };
