7067a61ee45ccc7ee15297b77a0948e02c78a6f2
rishabh verma <vermarishabh0987@gmail.com>
2020-05-09 23:29:33 +0530

    Updates

---
+++ precache-manifest.e77bc4c600b4cf68067d4c8396a7f4f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c30bda6b6ba0ff03f704b0382da09de",
+    "url": "/visualsortingV2/index.html"
+  },
+  {
+    "revision": "5585a0ceaa6089d9c19d",
+    "url": "/visualsortingV2/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "1649fc3c287e293b214c",
+    "url": "/visualsortingV2/static/css/main.d80c419c.chunk.css"
+  },
+  {
+    "revision": "5585a0ceaa6089d9c19d",
+    "url": "/visualsortingV2/static/js/2.0ed3283d.chunk.js"
+  },
+  {
+    "revision": "3e9fe304421a6de4d5448a9a52d993dd",
+    "url": "/visualsortingV2/static/js/2.0ed3283d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1649fc3c287e293b214c",
+    "url": "/visualsortingV2/static/js/main.71e56949.chunk.js"
+  },
+  {
+    "revision": "d46c448ee132f4bc3f97",
+    "url": "/visualsortingV2/static/js/runtime-main.6e35637c.js"
+  }
+]);
